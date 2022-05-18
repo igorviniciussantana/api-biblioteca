a3ffadcc46a79b51b4c3b8c80ac42a4c556b4824
igor vinicius <86114583+igorviniciussantana@users.noreply.github.com>
2022-05-18 21:21:26 +0000

    Criado Models Autor e Livro

---
+++ models/livro.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class livro extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  livro.init({
+    autorId: DataTypes.INTEGER,
+    titulo: DataTypes.STRING,
+    editora: DataTypes.STRING,
+    data_publicacao: DataTypes.DATE,
+    preco: DataTypes.REAL
+  }, {
+    sequelize,
+    modelName: 'livro',
+  });
+  return livro;
+};
