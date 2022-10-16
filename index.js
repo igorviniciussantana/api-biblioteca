d4a7ad2f0166828ab9267d051e5e4837263c4885
igor vinicius <igor.santana@estudante.ifms.edu.br>
2022-10-15 22:12:28 -0400

    feat: adicionado cors

--- index.js
+++ index.js
@@ -6,13 +6,22 @@ const PORT = process.env.PORT || 8877;
 var { autor } = require("./models");
 var { livro } = require("./models")
 
+var cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({
 
   extended: true
 }));
 
+
+app.use((req, res, next) => {
+  console.log('Acessou')
+  res.header("Access-Control-Allow-Origin", "*")
+  app.use(cors());
+  next();
+});
 
 app.get("/", async function(req, res) {
 
